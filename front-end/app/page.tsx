4fd0ae727fb5cc6ddc9cc39bad2c4027cffcd10f
Pedro Souza <pedro.souza4237@gmail.com>
2025-06-30 17:26:32 -0300

    Importing libraries and frameworks to the front end

---
+++ front-end/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
